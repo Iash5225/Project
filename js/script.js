76c34bf12eb11e58ffd1023379db31a263f43c47
Anonymoose-0 <david.holt2002@gmail.com>
2022-04-22 09:05:46 +0800

    Merge changes?

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(window).on('load', () => {
                 var cells = document.createElement("div");
 
                 //Set divs created to all have a classname "cell"
-                cells.className = "clickable";
+                cells.className = "unclicked";
 
                 //Generate random letter
                 const letter = generateRandomLetter()
@@ -31,23 +31,48 @@ $(window).on('load', () => {
     //Create the grid
     createGrid(40);
 
+    function createInput(){
+        var input = document.getElementsByClassName("input")[0];
+        console.log(input);
+        for(let j = 0 ; j < 5 ; j++){
+            let inputcell = document.createElement("div");
+            inputcell.className = "inputcells";
+            input.appendChild(inputcell);
+        }
+    }
+
+    createInput();
+
     //Storing every letter clicked
     var lettersClicked = [];
+    var count = 0;
 
     function clickedCell(){
+    if(count == 5){
+        alert("Already have 5 letters");
+        return;
+    }
+
         //Add to array
         lettersClicked.push(this.dataset.word);
 
-        //Declare it to be clicked
-        this.className = "clicked";
+        //Declare it to be clicked or unclicked
+        if(this.className == "unclicked"){
+            this.className = "clicked";
+        } else{
+            this.className = "unclicked";
+        }
         console.log(lettersClicked);
 
         //Call a function which inserts it into the input div
-        addToInput();
+        addToInput(this.dataset.word , count);
+        count++;
     }
 
-    function addToInput(){
-        console.log("Hi");
+    function addToInput(word , count){
+        var g = document.getElementsByClassName("inputcells");
+        console.log(count);
+        g[count].innerHTML = word;
     }
 
     //Generating a random letter
