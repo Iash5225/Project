3b0780875d7ab43a4138019408cdb3f50d1f9b39
Iash5225 <iash.bashir5225@gmail.com>
2022-04-24 20:10:45 +0800

    Stored every column into separate arrays, now adding row and colum property into every data cell

--- js/script.js
+++ js/script.js
@@ -10,6 +10,25 @@ $(window).on("load", () => {
       //Set divs created to all have a classname "cell"
       cells.className = "unclicked";
 
+      //adding index to every div in grid
+      cells.index = i;
+
+      if (i % 5 == 0) {
+        cells.column = 0;
+      }
+      if (i % 5 == 1) {
+        cells.column = 1;
+      }
+      if (i % 5 == 2) {
+        cells.column = 2;
+      }
+      if (i % 5 == 3) {
+        cells.column = 3;
+      }
+      if (i % 5 == 4) {
+        cells.column = 4;
+      }
+
       //Generate random letter
       const letter = generateRandomLetter();
 
@@ -32,7 +51,7 @@ $(window).on("load", () => {
 
   function createInput() {
     var input = document.getElementsByClassName("input")[0];
-    console.log(input);
+    // console.log(input);
     for (let j = 0; j < 5; j++) {
       let inputcell = document.createElement("div");
       inputcell.className = "inputcells";
@@ -62,7 +81,8 @@ $(window).on("load", () => {
       removeLetter(count);
       this.className = "unclicked";
     }
-    console.log(lettersClicked);
+     console.log(lettersClicked);
+     console.log(this.column);
 
     //Call a function which inserts it into the input div
     addToInput(this.dataset.word, count);
@@ -71,7 +91,7 @@ $(window).on("load", () => {
 
   function addToInput(word, count) {
     var g = document.getElementsByClassName("inputcells");
-    console.log(count);
+    // console.log(count);
     g[count].innerHTML = word;
   }
 
@@ -80,13 +100,13 @@ $(window).on("load", () => {
     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     return alphabet[Math.floor(Math.random() * alphabet.length)];
   }
-/** Code for Generating Trie:
- *  to update: 
- *    modify the array in wordlist.js
- *    uncomment the below code and reload the webpage
- *    copy and paste the output from the console to the trie.js replacing everything in the {}
- */ 
- 
+  /** Code for Generating Trie:
+   *  to update:
+   *    modify the array in wordlist.js
+   *    uncomment the below code and reload the webpage
+   *    copy and paste the output from the console to the trie.js replacing everything in the {}
+   */
+
   // const word_dict = new Trie();
 
   // for (let i = 0; i < wordlist.length; i++) {
@@ -96,13 +116,53 @@ $(window).on("load", () => {
 
   /* End of section */
 
-
   //Intialises RAW_TRIE from trie.js to a Trie datatype that can be searched at runtime
   const TRIE = Object.setPrototypeOf(RAW_TRIE, new Trie());
-  console.log("hello exists: " + TRIE.search("hello"));
-  console.log("hello1 does not exist: " + TRIE.search("hello1"));
+  // console.log("hello exists: " + TRIE.search("hello"));
+  // console.log("hello1 does not exist: " + TRIE.search("hello1"));
 
-  
+  storegrid(40);
+
+  // store the grid as a 2d array of letters
+
+  function storegrid(numrows) {
+    var grid = document.getElementsByClassName("grid")[0];
+    let col0 = [];
+    let col1 = [];
+    let col2 = [];
+    let col3 = [];
+    let col4 = [];
+
+    for (let i = 0; i < 40; i = i + 5) {
+      var cells = document.getElementsByClassName("unclicked")[i];
+      col0.push(cells.innerHTML);
+    }
+
+    for (let i = 1; i < 40; i = i + 5) {
+      var cells = document.getElementsByClassName("unclicked")[i];
+      col1.push(cells.innerHTML);
+    }
+
+    for (let i = 2; i < 40; i = i + 5) {
+      var cells = document.getElementsByClassName("unclicked")[i];
+      col2.push(cells.innerHTML);
+    }
+
+    for (let i = 3; i < 40; i = i + 5) {
+      var cells = document.getElementsByClassName("unclicked")[i];
+      col3.push(cells.innerHTML);
+    }
+
+    for (let i = 4; i < 40; i = i + 5) {
+      var cells = document.getElementsByClassName("unclicked")[i];
+      col4.push(cells.innerHTML);
+    }
+    console.log(col2);
+    console.log(col1);
+    console.log(col2);
+    console.log(col3);
+    console.log(col4);
+  }
 });
 
 /* Menu button */
