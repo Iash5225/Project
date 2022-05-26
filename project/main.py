c1559dbd13fd6a11305f996683b83c86286b3f9f
j-plebreton <21118434@student.uwa.edu.au>
2022-05-26 10:35:56 +0800

    implemented leaderboard db access

--- project/main.py
+++ project/main.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template
+import imp
+from select import select
+from flask import Blueprint, render_template, session
 from flask_login import login_required, current_user
 from .models import User, Scores
 from . import db
+from sqlalchemy import desc, select
 
 main = Blueprint('main', __name__)
 
@@ -12,4 +15,16 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.name, highscore=User.query.get(current_user.highscore))
+    leaderboard = db.session.execute(select(User.name, User.highscore)
+                                     .order_by(User.highscore, User.name)
+                                     .limit(10))
+    leaderboard_names = []
+    leaderboard_scores = []
+    for row in leaderboard:
+        leaderboard_names.append(row.name)
+        leaderboard_scores.append(row.highscore)
+        
+    return render_template('profile.html',
+                           name = current_user.name,
+                           lb_names = leaderboard_names,
+                           lb_scores = leaderboard_scores)
