cc1f8974f250823f21d13f0227ae92fde7de3e05
Iash5225 <iash.bashir5225@gmail.com>
2022-05-27 10:10:43 +0800

    Added Email Testing

--- test.py
+++ test.py
@@ -8,13 +8,12 @@ class UserModelCase(unittest.TestCase):
         self.app = app.test_client()     
         app.app_context().push()
         db.create_all
-        LastUser = last_item = User.query.order_by(User.id.desc()).first()
+        LastUser = User.query.order_by(User.id.desc()).first()
         
         U1 = User(id = LastUser.id +1, email="iash@iash", name ="iash", password = "1234");
         U2 = User(id = LastUser.id +2, email="jp@jp", name ="jp", password = "ilikeCITS3403");
         U3 = User(id = LastUser.id +3, email="david@david", name ="david", password = "idon'tlikeCITS3403");
         
-        #u1 = User(id = LastUser.id +1, email="bob@bob", name ="bob", password = "1234");
         db.session.add(U1)
         db.session.add(U2)
         db.session.add(U3)
@@ -22,13 +21,12 @@ class UserModelCase(unittest.TestCase):
 
     def tearDown(self):
         temp = User.query.order_by(User.id.desc()).first()
-        lastid = temp.id
-        User.query.filter_by(id=lastid).delete()
-        User.query.filter_by(id=lastid-1).delete()
-        User.query.filter_by(id=lastid-2).delete()
+        User.query.filter_by(id=temp.id).delete()
+        User.query.filter_by(id=temp.id-1).delete()
+        User.query.filter_by(id=temp.id-2).delete()
         db.session.commit()
 
-    def test_password_hashing(self):
+    def test_users(self):
         temp = User.query.order_by(User.id.desc()).first()
         
         U1 = User.query.get(temp.id-2)
@@ -37,18 +35,23 @@ class UserModelCase(unittest.TestCase):
         
         ##Test should return True
         self.assertEqual(U1.name,"iash")
+        self.assertEqual(U1.email,"iash@iash")
         self.assertEqual(U1.password,"1234")
         
         ##Test should return True
         self.assertEqual(U2.name,"jp")
+        self.assertEqual(U2.email,"jp@jp")
         self.assertEqual(U2.password,"ilikeCITS3403")
         
         ##Test should return True
         self.assertEqual(U3.name,"david")
+        self.assertEqual(U3.email,"david@david")
         self.assertEqual(U3.password,"idon'tlikeCITS3403")
         
-        ##Test should return False
-        self.assertEqual(U3.password,"test")
+        ##Test False values
+        self.assertNotEqual(U1.name,"tim")
+        self.assertNotEqual(U2.email,"bob@builder")
+        self.assertNotEqual(U3.password,"test")
         
 if __name__ == '__main__':
     unittest.main(verbosity=2)
