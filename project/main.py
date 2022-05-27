6c9c627de46a6fa531ce043a3b3614d533a6a95a
j-plebreton <21118434@student.uwa.edu.au>
2022-05-27 14:25:09 +0800

    added stats as var your displaying user scores

--- project/main.py
+++ project/main.py
@@ -15,16 +15,24 @@ def index():
 @main.route('/game')
 @login_required
 def game():
+    
+    # get user's score history from db to render personal stats
+    user_score_history = db.session.execute(select(Scores.score)
+                                            .filter_by(Scores.userid == current_user.userid)
+                                            .order_by(Scores.date))
+    
+    user_scores = [row.score for row in user_score_history]
+    
+    # get leaderboard from db to render leaderboard
     leaderboard = db.session.execute(select(User.name, User.highscore)
                                      .order_by(User.highscore, User.name)
                                      .limit(10))
-    leaderboard_names = []
-    leaderboard_scores = []
-    for row in leaderboard:
-        leaderboard_names.append(row.name)
-        leaderboard_scores.append(row.highscore)
+    
+    leaderboard_names = [row.name for row in leaderboard]
+    leaderboard_scores = [row.highscore for row in leaderboard]
         
     return render_template('game.html',
                            name = current_user.name,
                            lb_names = leaderboard_names,
-                           lb_scores = leaderboard_scores)
+                           lb_scores = leaderboard_scores,
+                           stats = user_scores)
