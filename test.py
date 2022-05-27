f62a741949650aaca4ba1e81b0d2d98b184bf189
Iash5225 <iash.bashir5225@gmail.com>
2022-05-27 09:05:32 +0800

    Adds Rows to existing DB and tests correctly

--- test.py
+++ test.py
@@ -1,22 +1,32 @@
-import email
+
+from flask import Flask
 import unittest, os
-from project import db,create_app
+from project import create_app, db,create_TEMPapp
 from project.models import User
 
 class UserModelCase(unittest.TestCase):
-    def setUp(self):
-        db.create_all(app=create_app())
+    def setUp(self):        
+        app = create_app()      
+        self.app = app.test_client()     
+        app.app_context().push()
+        db.create_all
+       # u1 = User(id = 8, email="caleb@caleb", name ="caleb", password = "dog");
+      #  db.session.add(u1)
+        db.session.commit()
 
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+    # def tearDown(self):
+      #  User.query.filter_by(id=5).delete()
+      #  db.session.commit()
+       # db.session.remove()
+      #  db.drop_all()
 
     def test_password_hashing(self):
        # u = User(username='susan')
-        u = User(id = "00000000", email="suzen@suzen");
-        u.password("cat")
-        self.assertFalse(u.check_password('dog'))
-        self.assertTrue(u.check_password('cat'))
+        u = User.query.get('5')
+        str2 = u.password
+        self.assertEqual(str2,"cat")
+        #self.assertEqual("cat","cat")
+
         
 if __name__ == '__main__':
     unittest.main(verbosity=2)
