514411b15bda7211d52405bcd99944743c2d7c02
Iash <iash.bashir5225@gmail.com>
2022-05-29 22:18:02 +0800

    Removed Initial Added users + scores

--- systemtests.py
+++ systemtests.py
@@ -19,12 +19,6 @@ class SystemTest(unittest.TestCase):
             self.app = app.test_client()     
             app.app_context().push()
             db.create_all()
-            
-            U1 = User(email="smith@smith",  password = generate_password_hash("1234", method='sha256'),name ="smith");
-            S1 = Scores( userid = 1, score = 10)
-        
-            db.session.add(U1)
-            db.session.add(S1)
 
             db.session.commit()
             self.driver.maximize_window()
@@ -38,68 +32,49 @@ class SystemTest(unittest.TestCase):
             LastScore = Scores.query.order_by(Scores.score_log_id.desc()).first()
             
             User.query.filter_by(id=LastUser.id).delete()
-            User.query.filter_by(id=LastUser.id-1).delete()
-        
-            Scores.query.filter_by(score_log_id=LastScore.score_log_id).delete()
         
             db.session.commit()
             self.driver.close()            
 
     def test_signup(self):
-        ##SIGN UP A USER
-        temp = User.query.order_by(User.id.desc()).first()
-        
-        U1 = User.query.get(temp.id)
-        
-        ##Test should return True
-        self.assertEqual(U1.name,"smith")
-        self.assertEqual(U1.email,"smith@smith")
-        self.assertTrue(check_password_hash(U1.password, "1234"))
-        
         self.driver.get('http://localhost:5000/signup')
         
         self.driver.implicitly_wait(5)
         
-        #Email = self.driver.find_element(by=By.CLASS_NAME, value="email")
-        Email = self.driver.find_element_by_id('email')
+        Email = self.driver.find_element(By.ID,'email')
         Email.send_keys('SystemTest@SystemTest')
         
-        #Name = self.driver.find_element(by=By.CLASS_NAME, value="name")
-        Name = self.driver.find_element_by_id('name')
+        Name = self.driver.find_element(By.ID,'name')
         Name.send_keys('SystemTest')
         
-       # Password = self.driver.find_element(by=By.CLASS_NAME, value="password")
-        Password = self.driver.find_element_by_id('password')
+        Password = self.driver.find_element(By.ID,'password')
         Password.send_keys('PasswordTester')
         
         self.driver.implicitly_wait(5)
                 
-        SignUpButton = self.driver.find_element_by_class_name("Login")
+        SignUpButton = self.driver.find_element(by=By.CLASS_NAME, value="Login")
         SignUpButton.click()  
 
         sleep(2)
 
         ##to check if user typing in wrong password
 
-        LoginButton = self.driver.find_element_by_class_name("Login")
+        LoginButton = self.driver.find_element(by=By.CLASS_NAME, value="Login")
 
-        Email = self.driver.find_element_by_id('email')
+        Email = self.driver.find_element(By.ID,'email')
         Email.send_keys('SystemTest@SystemTest')
 
-        Password = self.driver.find_element_by_id('password')
+        Password = self.driver.find_element(By.ID,'password')
         Password.send_keys('Wrong Password')
 
         sleep(2)
         
         LoginButton.click() 
 
-        ErrorMessage = self.driver.find_element_by_id('errormessage').get_attribute('innerHTML')
+        ErrorMessage = self.driver.find_element(By.ID,'errormessage')
+        self.assertEqual(ErrorMessage.get_attribute('innerHTML'),'Please check your login details and try again.')
         sleep(2)
 
-        #check login success
-        #self.driver.implicitly_wait(5)
-        #logout = self.driver.find_element_by_partial_link_text('Logout')
-        #self.assertEqual(logout.get_attribute('innerHTML'), 'Logout Testy', msg='Logged in')
  
 if __name__ == '__main__':
     unittest.main(verbosity=2)
