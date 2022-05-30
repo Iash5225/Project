c1ce9cfe1f73f2c3ebe4577f88c9f34b6b156e0a
j-plebreton <21118434@student.uwa.edu.au>
2022-05-30 10:05:34 +0800

    added stat vars to profile

--- project/main.py
+++ project/main.py
@@ -52,4 +52,29 @@ def game():
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html')
+        # get user's score history from db to render personal stats
+    user_score_history = db.session.execute(select(Scores.score, Scores.date)
+                                            .filter_by(userid=current_user.id)
+                                            .order_by(Scores.date.desc()))
+    user_scores = [[], []]
+    for row in user_score_history:
+        user_scores[0].append(row.score)
+        user_scores[1].append(row.date)
+
+
+    # get leaderboard from db to render leaderboard
+    leaderboard = db.session.execute(select(User.highscore, User.name)
+                                    .order_by(User.highscore.desc(), User.name)
+                                    .limit(10))
+
+    leaderboard_names = []
+    leaderboard_scores =[]
+    for row in leaderboard:
+        leaderboard_names.append(row.name)
+        leaderboard_scores.append(row.highscore)
+
+    return render_template('profile.html',
+                           name = current_user.name,
+                           lb_names = leaderboard_names,
+                           lb_scores = leaderboard_scores,
+                           user_scores = user_scores)
